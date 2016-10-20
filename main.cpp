f45daeb9cae37a22a3cf4478bae2d22a49c38b21
kostagiolasn <nikos.kostagiolas@gmail.com>
2016-10-20 22:13:28 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: nikos
+ *
+ * Created on October 20, 2016, 10:09 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
