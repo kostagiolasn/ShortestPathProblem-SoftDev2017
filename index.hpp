d2c1ece3f2324bf4370a6389410d652bc6ac26f9
kostagiolasn <nikos.kostagiolas@gmail.com>
2016-10-23 18:48:56 +0300

    Initialized index struct

---
+++ index.hpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   index.hpp
+ * Author: nikos
+ *
+ * Created on October 23, 2016, 6:46 PM
+ */
+
+#ifndef INDEX_HPP
+#define	INDEX_HPP
+
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+const size_t INITIAL_INDEX_SIZE = 512;
+const size_t INITIAL_BUFFER_SIZE = 512;
+const size_t INITIAL_BUFFER_ARRAY_SIZE = 10;
+
+
+
+#endif	/* INDEX_HPP */
+
