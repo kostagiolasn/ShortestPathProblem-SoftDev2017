0a7207efba53455d76c8aab1e26c5ebbc4bc6bcc
kostagiolasn <nikos.kostagiolas@gmail.com>
2016-10-23 18:14:44 +0300

    Finished setting up the parameters

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
  * Created on October 20, 2016, 10:09 PM
  */
 
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <cstdlib>
 
 using namespace std;
@@ -12,8 +15,54 @@ using namespace std;
 /*
  * 
  */
+
+void args_setup(int argc, char* argv[], std::string& fileGraph, std::string& fileWorkLoad);
+
 int main(int argc, char** argv) {
 
-    return 0;
+    std::string fileGraph;
+    std::string fileWorkLoad;
+    int state = 0;
+    
+    try {
+        args_setup(argc, argv, fileGraph, fileWorkLoad);
+    } catch (std::string err) {
+        std::cerr << err << std::endl;
+        state = 1;
+    }
+    
+    return state;
+}
+
+void args_setup(int argc, char* argv[], std::string& fileGraph, std::string& fileWorkLoad) {
+    /* Does main's dirty job */
+    int i, err = 0;
+    
+    fileGraph = "";
+    fileWorkLoad = "";
+    
+    for(i = 1; i < argc; i++) {
+        if( ! strcmp(argv[i], "-fGraph")) {
+            if( i > (argc - 2) ) {
+                err = 1;
+                break;
+            }
+            fileGraph = std::string(argv[i+1]);
+            i++;
+        } else if( ! strcmp(argv[i], "-fWorkLoad")) {
+            if( i > (argc - 2) ) {
+                err = 1;
+                break;
+            }
+            fileWorkLoad = std::string(argv[i+1]);
+            i++;
+        } else {
+            err = 1;
+            break;
+        }
+    }
+    if(err) {
+        throw std::string("Usage: " + std::string(argv[0]) + " -fGraph <Graph File> -fWorkLoad <Work Load File>");
+    }
 }
 
