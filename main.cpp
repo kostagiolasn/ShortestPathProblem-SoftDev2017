da47b950509634d8d765fc7ef44afd8af7ff7370
panagiotappl <panagiotappl@gmail.com>
2016-11-06 19:00:34 +0200

    unit test update

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
     }
 
     
-    //printGraph(&indexExternal, bufferExternal);
+    printGraph(&indexExternal, bufferExternal);
     //printGraph(&indexInternal, bufferInternal);
 
     
